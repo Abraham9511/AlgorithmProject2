0b19220f76e2eb16a6b76152c13481f1d6123fd5
xujiachen <772090945@qq.com>
2016-12-10 22:36:10 +0800

    Update Enumeration.h

--- src/Enumeration.h
+++ src/Enumeration.h
@@ -7,7 +7,9 @@
 
 const int BACK = 10;
 
-
+//输入：所有图像，图像的数量
+//输出：前BACK个最优的解
+//功能：使用暴力枚举算法找出最优的那些解，验证投影算法的准确度
 std::vector<std::pair<int, int> >Enumeration(Image* image, const int& image_size);
 
 
